e2f68896f2d82ca41d92b28fbbc90a638b46e5cf
Joe Khoobyar <joe@ankhcraft.com>
2009-04-30 17:51:34 -0400

    try the environment variable first

--- lib/net/ssh/askpass.rb
+++ lib/net/ssh/askpass.rb
@@ -1,8 +1,24 @@
 require 'net/ssh'
+require 'net/ssh/authentication/methods/keyboard_interactive'
 
 module Net
   module SSH
     module AskPass
+      module Prompt
+        def prompt(text, echo=true)
+          if ssh_askpass = ENV['SSH_ASKPASS']
+            `#{ssh_askpass} "#{text}"`
+          else
+            prompt_console text, echo
+          end
+        end
+      end    
+    end
+
+    Authentication::Methods::KeyboardInteractive.class_eval do
+      alias :prompt_console :prompt
+      include Net::SSH::AskPass::Prompt
+      alias :prompt_askpass :prompt
     end
   end
 end
