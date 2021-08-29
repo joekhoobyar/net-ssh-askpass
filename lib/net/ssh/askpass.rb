f71d6b5fe19c3cef910161fe7b45c65e9d3f23d9
Edwin Pratomo <edwin@insep.si>
2021-08-29 12:08:46 +0700

    made it work with net-ssh 5.x, 6.x

--- lib/net/ssh/askpass.rb
+++ lib/net/ssh/askpass.rb
@@ -1,32 +1,13 @@
 require 'net/ssh'
-require 'net/ssh/authentication/methods/keyboard_interactive'
 
-module Net
-  module SSH
-    module AskPass
-      module Prompt
-        def prompt(text, echo=true)
-          if ssh_askpass = ENV['SSH_ASKPASS']
-            `#{ssh_askpass} "#{text}"`.strip
-          else
-            prompt_console text, echo
-          end
-        end
-      end    
-    end
+Net::SSH::Prompt::Prompter.class_eval do
+  alias :ask_console :ask
 
-    Authentication::Methods::KeyboardInteractive.class_eval do
-      alias :prompt_console :prompt
-      include Net::SSH::AskPass::Prompt
-      alias :prompt_askpass :prompt
-    end
-
-    KeyFactory.class_eval do
-      class << self
-        alias :prompt_console :prompt
-        include Net::SSH::AskPass::Prompt
-        alias :prompt_askpass :prompt
-      end
+  def ask(prompt, echo=true)
+    if ssh_askpass = ENV['SSH_ASKPASS']
+      `#{ssh_askpass}`.strip
+    else
+      ask_console prompt, echo
     end
   end
 end
