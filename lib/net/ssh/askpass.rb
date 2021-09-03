5d07b1a212efa16dac9ec0399f9be729e3f8f694
Edwin Pratomo <edwin.insep.si>
2021-09-03 14:51:38 +0700

    added support to older net-ssh

--- lib/net/ssh/askpass.rb
+++ lib/net/ssh/askpass.rb
@@ -1,13 +1,37 @@
 require 'net/ssh'
 
-Net::SSH::Prompt::Prompter.class_eval do
-  alias :ask_console :ask
-
-  def ask(prompt, echo=true)
-    if ssh_askpass = ENV['SSH_ASKPASS']
-      `#{ssh_askpass}`.strip
-    else
-      ask_console prompt, echo
+module Net
+  module SSH
+    module AskPass
+      module Prompt
+        def prompt(text, echo=true)
+          if ssh_askpass = ENV['SSH_ASKPASS']
+            `#{ssh_askpass}`.strip
+          else
+            prompt_console text, echo
+          end
+        end
+      end
     end
+  end
+end
+
+if Net::SSH::Prompt.method_defined?(:prompt)
+  Net::SSH::Authentication::Methods::KeyboardInteractive.class_eval do
+    alias :prompt_console :prompt
+    include Net::SSH::AskPass::Prompt
+  end
+
+  Net::SSH::KeyFactory.class_eval do
+    class << self
+      alias :prompt_console :prompt
+      include Net::SSH::AskPass::Prompt
+    end
+  end
+else
+  Net::SSH::Prompt::Prompter.class_eval do
+    alias :prompt_console :ask
+    include Net::SSH::AskPass::Prompt
+    alias :ask :prompt
   end
 end
